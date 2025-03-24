adb1dd3c1f6951021216670b88f7deddc587d4b9
yuriBean <elizabetheden1415@gmail.com>
2025-03-24 09:22:06 +0500

    afflink99cors

--- src/app/api/store-utm/route.js
+++ src/app/api/store-utm/route.js
@@ -2,6 +2,16 @@ import { NextResponse } from "next/server";
 
 let storedUTMs = {};
 
+export async function OPTIONS() {
+    return NextResponse.json({}, { 
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      }
+    });
+  }
+
 export async function POST(req) {
   try {
     const { utm_source, utm_medium, utm_campaign, utm_product, utm_content, utm_commission_rate, utm_affiliate_network } = await req.json();
